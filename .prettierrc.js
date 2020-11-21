3e887a2be913728cb5a8f4355983c91b5403218f
Dmitry Usik <d.usik@riseapps.biz>
2020-11-21 14:09:02 +0200

    - configure prettier;

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    printWidth: 100,
+    singleQuote: true,
 };
