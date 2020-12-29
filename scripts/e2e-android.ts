02915c1bc48d50b3731ac875a3e1aef093ab3af3
Dmitry Usik <d.usik@riseapps.biz>
2020-12-29 20:20:40 +0200

    - fixed bundle issues for android;

--- scripts/e2e-android.ts
+++ scripts/e2e-android.ts
@@ -1,19 +1,9 @@
 import { exec } from 'shelljs';
 
-import e2e from '../config/e2e';
-
-const withSimulator = process.argv.includes('--withSimulator');
-
-const openSimulator = () =>
-  exec(`emulator -avd ${e2e.androidDeviceName} -no-snapshot -memory 2048 -no-boot-anim`, {
-    async: true,
-  });
+const startReactNative = () => exec('react-native start', { async: true });
 const detoxBuild = () => exec('detox build --configuration android.emu.debug');
 const detoxTest = () => exec('detox test --configuration android.emu.debug');
 
-if (withSimulator) {
-  openSimulator();
-}
-
+startReactNative();
 detoxBuild();
 detoxTest();
